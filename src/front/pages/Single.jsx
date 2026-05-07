7afc308759fa41b251e21ccd61d64edc180e8747
erikaportes <152933693+erikaportes@users.noreply.github.com>
2026-05-07 02:33:31 +0000

    guardado

--- src/front/pages/Single.jsx
+++ src/front/pages/Single.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";  // To define prop types for this component
 import rigoImageUrl from "../assets/img/rigo-baby.jpg"  // Import an image asset
 import useGlobalReducer from "../hooks/useGlobalReducer";  // Import a custom hook for accessing the global state
 
+
 // Define and export the Single component which displays individual item details.
 export const Single = props => {
   // Access the global state using the custom hook.
